89136dd0d15077a511ea90411f396edbb777d32f
Hicham <hello@hicham.party>
2024-11-28 18:58:42 +0100

    Improve MIDI incoming messages

--- lib/IO/MidiSerial.cpp
+++ lib/IO/MidiSerial.cpp
@@ -11,22 +11,43 @@ void MidiSerial::setup()
     MidiUART.begin(MIDI_CHANNEL_OMNI);
 }
 
-void MidiSerial::loop(Scene *scene)
+void MidiSerial::loop(Scene *currentScene)
 {
     // Read incoming MIDI messages
     while (MidiUART.read())
     {
         // Handle the MIDI message
         byte type = MidiUART.getType();
-        byte channel = MidiUART.getChannel();
-        byte note = MidiUART.getData1();
-        byte velocity = MidiUART.getData2();
 
-        if (type == 0x90)
+        if (type == MidiType::NoteOn)
         {
-            scene->onNotePlayed(note, velocity);
+            byte channel = MidiUART.getChannel();
+            byte note = MidiUART.getData1();
+            byte velocity = MidiUART.getData2();
+            currentScene->onNotePlayed(note, velocity);
+            debug(1, "[midi] Received note on channel %02X: %02X %02X %02X", channel, type, note, velocity);
         }
+        else if (type == MidiType::SystemExclusive)
+        {
+            byte channel = MidiUART.getChannel();
+            const byte *array = MidiUART.getSysExArray();
+            unsigned size = MidiUART.getSysExArrayLength();
 
-        debug(1, "[midi] Received MIDI message: %02X %02X %02X %02X", type, channel, note, velocity);
+            Serial.print("[midi] System Exclusive: ");
+
+            for (int i = 0; i < size; i++)
+            {
+                Serial.print(array[i], HEX);
+                Serial.print(" ");
+            }
+            Serial.println();
+        }
+        else if (type == MidiType::ControlChange)
+        {
+            byte channel = MidiUART.getChannel();
+            byte control = MidiUART.getData1();
+            byte value = MidiUART.getData2();
+            debug(1, "[midi] Received control change: %02X %02X %02X", type, control, value);
+        }
     }
 }
