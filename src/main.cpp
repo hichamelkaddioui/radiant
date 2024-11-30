d06c4d906e10b42c4c50f9d1ff02d4f9fb260d84
Hicham <hello@hicham.party>
2024-11-30 20:24:42 +0100

    WIP: read write banks to flash

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Led.h>
 #include <NeoPixel.h>
 #include <Graph.h>
+#include <Flash.h>
 #include <Utils.h>
 
 MidiSerial midiSerial;
@@ -30,96 +31,97 @@ void setup1()
     // OLED screen
     screen.setup();
 
-    /**
-     * LED SETUP + SERIALIZE AND DESERIALIZE
-     */
+    // Flash setup
+    RP2040Flash flash;
+    flash.begin();
 
+    /**
+     * TEST OF SERIALIZATION + FLASH
+     * 
+     * Create local banks, serialize them, and write everything to flash
+     */
+    size_t offset = 0, length = 0;
+    uint8_t buffer[1024]{};
+
+    // LedBank serialization
     LedBank localLedBank;
     NeoPixel *pixel = new NeoPixel(16);
     localLedBank._bank[0] = pixel;
 
-    size_t bitsInBuffer = 0;
-    uint8_t buffer[1024]{};
-
-    // LedBank serialization
-    bitsInBuffer = localLedBank.serialize(buffer);
-    debug(1, "[setup] serialized %d bits for led bank", bitsInBuffer);
+    length = localLedBank.serialize(buffer);
+    debug(1, "[setup] [led bank] serialized %d bits", length);
+    offset += length;
 
     localLedBank.clear();
 
-    bitsInBuffer = lb.deserialize(buffer);
-    debug(1, "[setup] deserialized %d bits for led bank", bitsInBuffer);
-
-    // LedBank setup
-    lb.setup();
-
-    /**
-     * GRAPH BANK SERIALIZATION
-     */
-
-    // Create a new graph bank and add a graph in it
+    // GraphBank serialization
     GraphBank localGraphBank = defaultGraphBank();
     localGraphBank._bank[9] = new GraphKeyframe({Keyframe(0.0f, 0.13f, 0.0f), Keyframe(1.0f, 0.12f, 0.0f)});
 
+    length = localGraphBank.serialize(buffer + offset);
+    debug(1, "[setup] [graph bank] serialized %d bits", length);
+    offset += length;
+
+    // SceneBank serialization
+    Sequence *hue = new Sequence(
+        {gb._bank[DefaultGraph::SINE], 0, 255, 9 * 1000},
+        PlaybackMode::REPEAT,
+        {61});
+    Sequence *brightness = new Sequence(
+        {gb._bank[DefaultGraph::SINE], 50, 100, 10 * 1000, 0.5f},
+        PlaybackMode::REPEAT);
+
+    LedEffect *pixelEffect = new LedEffect(lb._bank[0], hue, hue, brightness, brightness);
+
+    Scene *firstScene = new Scene();
+    firstScene->_ledEffects.push_back(*pixelEffect);
+
+    SceneBank localSceneBank;
+    localSceneBank._scenes.push_back(firstScene);
+
+    length = localSceneBank.serialize(buffer + offset, &lb, &gb);
+    debug(1, "[setup] [scene bank] serialized %d bits", length);
+    offset += length;
+
+    // Write to flash
+    flash.write(0x0, buffer, offset);
+
     // Reset buffer
+    offset = 0, length = 0;
     memset(buffer, 0, 1024);
 
-    // Serialize
-    bitsInBuffer = localGraphBank.serialize(buffer);
-    debug(1, "[setup] [graph bank] serialized %d bits", bitsInBuffer);
+    // Read from flash
+    flash.read(0x0, buffer, 1024);
 
-    // Create a new graph bank
+    // DESERIALIZE
+
+    // LedBank setup
+    length = lb.deserialize(buffer);
+    debug(1, "[setup] deserialized %d bits for led bank", length);
+    offset += length;
+
+    lb.setup();
+
+    // GraphBank setup
     gb = defaultGraphBank();
     debug(1, "[setup] [graph bank] number of graphs in new graph bank before: %d", gb._bank.size());
-
-    // Deserialize in the new graph bank
-    bitsInBuffer = gb.deserialize(buffer);
-    debug(1, "[setup] [graph bank] deserialized %d bits", bitsInBuffer);
+    length = gb.deserialize(buffer + offset);
+    debug(1, "[setup] [graph bank] deserialized %d bits", length);
     debug(1, "[setup] [graph bank] number of graphs in new graph bank after: %d", gb._bank.size());
+    offset += length;
 
     // Print value at 0.0f and 1.0f
     GraphKeyframe *keyframeGraph = dynamic_cast<GraphKeyframe *>(const_cast<Graph *>(gb._bank[9]));
     if (keyframeGraph != nullptr)
         debug(1, "[setup] number of keyframes in graph #9: %d, value at 0.0f: %f, value at 1.0f: %f", keyframeGraph->_keyframes.size(), keyframeGraph->valueAt(0.0f), keyframeGraph->valueAt(1.0f));
 
-    /**
-     * SCENE SETUP
-     */
-
-    // Local scene bank
-    SceneBank sbLocal;
-
-    Sequence *hue = new Sequence(
-        {gb._bank[DefaultGraph::SINE], 0, 255, 9 * 1000},
-        PlaybackMode::REPEAT,
-        {61});
-
-    Sequence *brightness = new Sequence(
-        {gb._bank[DefaultGraph::SINE], 50, 100, 10 * 1000, 0.5f},
-        PlaybackMode::REPEAT);
-
-    LedEffect *pixelEffect = new LedEffect(lb._bank[0], hue, hue, brightness, brightness);
-    Scene *firstScene = new Scene();
-    firstScene->_ledEffects.push_back(*pixelEffect);
-
-    sbLocal._scenes.push_back(firstScene);
-
-    // Reset buffer
-    memset(buffer, 0, 1024);
-
-    sbLocal.getCurrentScene()->dump();
-
-    // Scene serialization
-    bitsInBuffer = sbLocal.serialize(buffer, &lb, &gb);
-    debug(1, "[setup] [scene bank] serialized %d bits", bitsInBuffer);
-
-    // Scene deserialization
-    bitsInBuffer = sb.deserialize(buffer, &lb, &gb);
-    debug(1, "[setup] [scene bank] deserialized %d bits", bitsInBuffer);
+    // SceneBank deserialization
+    length = sb.deserialize(buffer + offset, &lb, &gb);
+    debug(1, "[setup] [scene bank] deserialized %d bits", length);
 
     sb.getCurrentScene()->dump();
 
-    // Scene setup
+    // Done!
     debug(1, "[setup] Setup complete");
 }
 
